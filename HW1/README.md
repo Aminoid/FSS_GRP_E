e2d8e682f4895f69cd83f555b80f545d7d6f0c51
Amit Kanwar <kanwaramit92@gmail.com>
2017-08-29 16:27:44 -0400

    Moving Repo to public one

---
+++ HW1/README.md
@@ -0,0 +1,20 @@
+# Homework 1
+This is **python** code for Homework 1.
+
+[1]: https://raw.githubusercontent.com/lualure/src/master/data/POM3A.csv
+
+## Problem Statement
+Read each line, kill whitepsace and anything after comment characters (#), break each line on comma, read rows into a list of lists (one list per row), converting strings to numbers where appropriate. Note that some column headers contain ?: all such columns should be ignored. For now you can ignore the other magic characters in row1.
+
+Your code should contain checks for bad lines (and bad lines should be skipped over); i.e. symbols where numbers should be and wrong number of cells (we will say that row1 has the “right” length).
+
+## Setup
+All the code is run on `python 2.7`.
+Download the [POM3A.csv][1] file and keep it in this folder only.
+
+## Running
+File can be run using:
+
+```python
+python hw1.py
+```
---
+++ HW1/hw1.py
@@ -0,0 +1,63 @@
+import os
+import sys
+import time
+
+start = time.time()
+
+row_index = 1
+
+def format_value(value):
+    try:
+        fval = float(value)
+        if fval.is_integer():
+            return int(value)
+        else:
+            return fval
+    except ValueError:
+        return value
+
+def show_error(message):
+    print "[Error] Line %d: %s" %(row_index, message)
+    sys.exit(0)
+
+def sanitize_line(line, f):
+    global row_index
+    # Check for comments and commas
+    temp = line.split("#")[0].rstrip()
+    while (temp[-1] == ','):
+        row_index += 1
+        temp += f.readline()
+        temp = temp.split("#")[0]
+
+    return temp, f
+
+with open("POM3A.csv", "rb") as fp:
+    ignore_cols = []
+    num_cols = 0
+
+    header, fp = sanitize_line(fp.readline(), fp)
+    cols = header.split(",")
+    num_cols = len(cols)
+    for index, value in enumerate(cols):
+        if "?" in value:
+            ignore_cols.append(index)
+
+    while True:
+        line = fp.readline()
+        if line == '':
+            break
+        sanitized, fp = sanitize_line(line, fp)
+        print sanitized
+        cols = sanitized.split(',')
+        row = []
+        if len(cols) != num_cols:
+            show_error("Mismatch in number of cols w.r.t header")
+        for index, value in enumerate(cols):
+            if index not in ignore_cols:
+                row.append(format_value(value))
+        print row
+
+end = time.time()
+
+print "=" * 40
+print "Time taken: %f" %(end - start)
