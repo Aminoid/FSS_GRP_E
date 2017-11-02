43613d7e2263a38645a705045607a197a4b276c2
Rahul <rmgutal@ncsu.edu>
2017-11-01 21:23:04 -0400

    taking the input file path from user

--- HW5/hw5.py
+++ HW5/hw5.py
@@ -4,20 +4,19 @@ import SdTree as TREE
 import sys
 import Tbl
 
-x = TREES.auto("dom")
-TREE.tprint(x)
-b=CON.branches(x)
-print "Number of branches: ", len(b)
-print "********************Plans********************"
-CON.plans(b)
-print
-print "********************Monitors********************"
-CON.monitors(b)
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python hw2.py <scv_file>")
+        sys.exit(0)
+    else:
+        file = sys.argv[1]
+        x = TREES.auto("dom")
+        TREE.tprint(x)
+        b=CON.branches(x)
+        print "Number of branches: ", len(b)
+        print "********************Plans********************"
+        CON.plans(b)
+        print
+        print "********************Monitors********************"
+        CON.monitors(b)
 
-#if __name__ == "__main__":
-    #if len(sys.argv) != 2:
-    #    print("Usage: python hw2.py <scv_file>")
-    #    sys.exit(0)
-    #else:
-        #file = "/home/rahulgutal4/auto.csv"
-        #TREE(file, "dom")
